6a810a170e1b53528ae2fd29910f68bdc7cd388c
Weathers92 <brandon.weathers92@gmail.com>
2017-04-28 12:54:35 -0600

    Added training and test data, example submission file, and .R file

---
+++ titanic.R
@@ -0,0 +1,11 @@
+#GIT commands to upload changes:
+#git status
+#git add .
+#git status
+#git commit -m "edit r file"
+#git status
+#git push origin master
+
+#clear working directory
+rm(list = ls())
+
