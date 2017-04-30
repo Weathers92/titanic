58686ac7dbd7e54fee9812eb49954779a5d694d1
Weathers92 <brandon.weathers92@gmail.com>
2017-04-30 16:26:01 -0600

    6 new predictions with updated r and work space.

--- titanic.R
+++ titanic.R
@@ -19,6 +19,8 @@ library(data.table)
 train <- fread("data/train.csv")
 test <- fread("data/test.csv")
 
+train$Survived <- as.factor(train$Survived)
+
 ######################
 ### Missing Values ###
 ######################
@@ -39,6 +41,14 @@ combined <- rbind(tempTrain, test)
 #Remove variables: PassengerId, Name, and Ticket
 combined <- subset(combined, select = -c(PassengerId, Name, Ticket))
 
+#Create a new column indicating how many cabins a person purchased.
+combined$nCabin <- 0
+
+for(i in 1:nrow(combined))
+{
+  combined$nCabin[i] <- length(strsplit(x = combined$Cabin[i], split = " ")[[1]])
+}
+
 #Change blanks to none.
 for(i in 1:nrow(combined))
 {
@@ -46,6 +56,18 @@ for(i in 1:nrow(combined))
   {
     combined$Cabin[i] = "none"
   }
+  if(combined$Embarked[i] == "")
+  {
+    combined$Embarked[i] = NA
+  }
+}
+
+#Replace each of the cabin numbers with just their levels.
+alpha <- c("A", "B", "C", "D", "E", "F", "G")
+
+for(i in 1:length(alpha))
+{
+  combined$Cabin[grepl(alpha[i], combined$Cabin, ignore.case = FALSE)] <- alpha[i]
 }
 
 #Coerce into factor variable.
@@ -53,6 +75,7 @@ combined$Pclass <- as.factor(combined$Pclass)
 combined$Sex <- as.factor(combined$Sex)
 combined$Cabin <- as.factor(combined$Cabin)
 combined$Embarked <- as.factor(combined$Embarked)
+
 
 #Coerce into numeric variable.
 combined$SibSp <- as.numeric(combined$SibSp)
@@ -68,7 +91,7 @@ completed1 <- complete(imputeVals1, 1)
 compTest1 <- completed1[892:1309,]
 compTrain1 <- completed1[1:891,]
 compTrain1 <- cbind(compTrain1, train$Survived)
-names(compTrain1)[9] <- "Survived"
+names(compTrain1)[10] <- "Survived"
 
 imputeVals2 <- mice(combined, m = 50, method = 'rf', seed = 0692)
 
@@ -77,7 +100,7 @@ completed2 <- complete(imputeVals2, 1)
 compTest2 <- completed2[892:1309,]
 compTrain2 <- completed2[1:891,]
 compTrain2 <- cbind(compTrain2, train$Survived)
-names(compTrain2)[9] <- "Survived"
+names(compTrain2)[10] <- "Survived"
 
 ######################
 ### Random Forests ###
@@ -86,8 +109,57 @@ names(compTrain2)[9] <- "Survived"
 library(randomForest)
 
 rf1 <- randomForest(Survived ~ ., importance = TRUE, data = compTrain1, set.seed(0692))
-varImpPlot(price.rf, scale = FALSE)
+varImpPlot(rf1, scale = FALSE)
+rfpred1 <- predict(rf1, compTest1)
+write.csv(rfpred1, file = "titanic_survivalrf1.csv")
+#Your submission scored 0.73206.
+#Position 6269.
 
 rf2 <- randomForest(Survived ~ ., importance = TRUE, data = compTrain2, set.seed(0692))
+varImpPlot(rf2, scale = FALSE)
+rfpred2 <- predict(rf2, compTest2)
+write.csv(rfpred2, file = "titanic_survivalrf2.csv")
+#Your submission scored 0.74163.
+#Position 6155.
+
+###############################
+### Support Vector Machines ###
+###############################
+
+library(e1071)
+
+svm1 <- svm(formula = Survived ~ ., data = compTrain1)
+svm1.pred <- predict(svm1, compTest1)
+write.csv(svm1.pred, file = "titanic_survivalsvm1.csv")
+#Your submission scored 0.77512.
+#Position 3988.
+
+svm2 <- svm(formula = Survived ~ ., data = compTrain2)
+svm2.pred <- predict(svm2, compTest2)
+write.csv(svm2.pred, file = "titanic_survivalsvm2.csv")
 
 
+#Untuned Support Vector Machines with pmm for imputation.
+
+#####################################
+### TUNED SUPPORT VECTOR MACHINES ###
+#####################################
+
+#Perform a grid search
+tuneResult1 <- tune(svm, Survived ~ ., data = compTrain1, 
+                   ranges = list(epsilon = seq(0, 1, 0.1), cost = 2^(2:9)))
+print(tuneResult1)
+plot(tuneResult1)
+tunedModel1 <- tuneResult1$best.model
+
+tsvm1.pred <- predict(tunedModel1, compTest1)
+write.csv(tsvm1.pred, file = "titanic_survivaltsvm1.csv")
+
+tuneResult2 <- tune(svm, Survived ~ ., data = compTrain2, 
+                    ranges = list(epsilon = seq(0, 1, 0.1), cost = 2^(2:9)))
+print(tuneResult2)
+plot(tuneResult2)
+tunedModel2 <- tuneResult2$best.model
+
+tsvm2.pred <- predict(tunedModel2, compTest2)
+write.csv(tsvm2.pred, file = "titanic_survivaltsvm2.csv")
